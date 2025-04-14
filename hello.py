aab660117fe5d6b3e4d26fe63b1fe28b1158bb3c
aibukrid <115988571+aibukrid@users.noreply.github.com>
2025-04-14 12:26:35 +0300

    Programm

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
-#my_name = input('Введите Ваше имя: ')
-#year = input('Введите какой сейчас год: ')
-#year_of_birth=input('Введите Ваш год рождения: ')
-#age = (int(year)-int(year_of_birth))
-#print('Меня зовут:',my_name,'.''\n''Сейчас идёт',year,'год.''\n''Я родился в',year_of_birth,'.''\n''Мне',age,'.')
+my_name = input('Введите Ваше имя: ')
+year = input('Введите какой сейчас год: ')
+year_of_birth=input('Введите Ваш год рождения: ')
+age = (int(year)-int(year_of_birth))
+print('Меня зовут:',my_name,'.''\n''Сейчас идёт',year,'год.''\n''Я родился в',year_of_birth,'.''\n''Мне',age,'.')
 
-from math import ceil
-time = input('Сколько минут прошло с начала дня: ')
-h = (int(time) // 60)
-a = (int(time) / 60 - h)
-min: int = ceil(a)
-print(h,':',min)
+#from math import ceil
+#time = input('Сколько минут прошло с начала дня: ')
+#h = (int(time) // 60)
+#a = (int(time) / 60 - h)
+#min: int = ceil(a)
+#print(h,':',min)
